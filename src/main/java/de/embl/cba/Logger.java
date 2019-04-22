755f3583c4d44e33ca4f8f7c6e29463f6c8f1499
Christian Tischer <christian.tischer@embl.de>
2019-04-22 20:35:16 +0200

    Initial commit

---
+++ src/main/java/de/embl/cba/Logger.java
@@ -0,0 +1,15 @@
+package de.embl.cba;
+
+public class Logger
+{
+	public void info( String txt )
+	{
+		System.out.println( txt );
+	}
+
+	public void progress( String txt )
+	{
+		System.out.println( txt );
+	}
+
+}
---
+++ src/test/java/headless/RunRF.java
@@ -0,0 +1,11 @@
+package headless;
+
+import de.embl.cba.classifiers.weka.FastRandomForest;
+
+public class RunRF
+{
+	public static void main( String[] args )
+	{
+		final FastRandomForest fastRandomForest = new FastRandomForest();
+	}
+}
